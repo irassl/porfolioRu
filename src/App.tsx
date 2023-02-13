1010bbc641e4c2a0481e8e2040fb8e69678a3c99
rassl <smith.ya-s@yandex.ru>
2023-02-13 15:12:38 +0300

    contact

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,27 @@ import {Home} from "./components/home/Home";
 import {ClassicHeader} from "./components/header/ClassicHeader";
 import {About} from "./components/about/About";
 import {Services} from "./components/service/Services";
+import {Portfolio} from "./components/porfolio/Portfolio";
+import {Resume} from "./components/resume/Resume";
+import {Contact} from "./components/contact/Contact";
+import {Footer} from "./components/footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-        <div id='content' role='main'>
-            <ClassicHeader/>
-            <Home/>
-            <About/>
-            <Services/>
+    return (
+        <div className="App">
+            <div id='content' role='main'>
+                <ClassicHeader/>
+                <Home/>
+                <About/>
+                <Services/>
+                <Portfolio/>
+                <Resume/>
+                <Contact/>
+            </div>
+            <Footer/>
+
         </div>
-
-
-    </div>
-  );
+    );
 }
 
 export default App;
